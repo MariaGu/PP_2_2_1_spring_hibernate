5e1604e2ddc118a4aff00f47af04e090abbb3e0c
gubina_mariia <girlfrombaikal@gmail.com>
2022-04-26 20:21:16 +0300

    @transactional changes, commented code is deleted

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -1,14 +1,10 @@
 package hiber.dao;
 
-import hiber.model.Car;
 import hiber.model.User;
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import java.util.List;
 
@@ -31,15 +27,7 @@ public class UserDaoImp implements UserDao {
     }
 
     @Override
-    @Transactional
     public User getUserByCar(String model, int series) {
-
-//        String hql = "SELECT u FROM User u INNER JOIN Car c ON c.id = u.car.id WHERE c.model = :modelName AND c.series = :seriesName";
-//        Session session = sessionFactory.getCurrentSession();
-//        Query query = session.createQuery(hql);
-//        query.setParameter("modelName", model);
-//        query.setParameter("seriesName", series);
-//        User user = (User) query.getSingleResult();
 
         TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery(
                 "from User u WHERE u.car.model = :modelName AND u.car.series = :seriesName");
